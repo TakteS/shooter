7466fb00ef80ebb967374b57f6db473111426492
TakteS <takt96@gmail.com>
2021-01-08 16:54:30 +0400

    Resize images, decrease input throttle

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :shooter, ShooterWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :error
 
 # ## SSL Support
 #
